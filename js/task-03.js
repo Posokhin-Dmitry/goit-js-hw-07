334ce910a65947417226541471db3b43a126d932
Dmitry Posokhin <79544402+NEGANN@users.noreply.github.com>
2021-08-07 23:53:25 +0300

    finish

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,14 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-];
+]
+
+const galleryEl = document.querySelector('#gallery')
+galleryEl.classList.add('gallery')
+const imagesMarkup = images
+  .map(image => {
+    return `<li><img src=${image.url} alt=${image.alt} width=300></li>`
+  })
+  .join(' ')
+
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup)
